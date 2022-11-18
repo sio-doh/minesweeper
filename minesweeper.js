43822189afd60528a745907e81884260a5e014ee
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-18 15:33:11 +0100

    mine positions

--- minesweeper.js
+++ minesweeper.js
@@ -9,19 +9,43 @@ const TILE_STATUSES = {
 
 export function createBoard(boardSize, numberOfMines) {
     const board = [] 
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+
     for (let x = 0; x < boardSize; x++) { 
         const row = []
         for (let y = 0; y < boardSize; y++) {
             const element = document.createElement("div")
             element.dataset.status = TILE_STATUSES.HIDDEN
+
             const tile = { 
                 element, 
                 x, 
-                y,
+                y, 
+                mine: true, 
+                get status() {
+                    return this.element.dataset.status
+                }, 
+                set status(value) {
+                    this.element.dataset.status = value 
+                }
             }
+
             row.push(tile)
         }
         board.push(row)
     } 
     return board
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+    
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize), 
+            y: randomNumber(boardSize)
+        }
+    }
+
+    return positions
 }
--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const NUMBER_OF_MINES = 2
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector(".board") 
-boardElement.style.setProperty("--size", BOARD_SIZE)
+console.log(board)
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
     })
 })
+boardElement.style.setProperty("--size", BOARD_SIZE)
 // 1. Populate a board with tiles and mines 
 // 2. Left-click on tiles to reveal tiles 
 // 3. Right-click on tiles to mark tiles 
 // 4. Check for a win / lose 
-
