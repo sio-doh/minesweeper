8a9a35c281eaeabeb6109b9ea861f2d7a6ec6052
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-18 13:28:48 +0100

    files needed for logic and UI

---
+++ script.js
@@ -0,0 +1,6 @@
+// User-Interface 
+
+// 1. Populate a board with tiles and mines 
+// 2. Left-click on tiles to reveal tiles 
+// 3. Right click on tiles to mark tiles 
+// 4. Check for a win / lose
